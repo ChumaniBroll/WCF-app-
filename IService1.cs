1f60aa8a605e8cd5d6f1e9208f6e20ea31da533a
ChumaniBroll <50732882+ChumaniBroll@users.noreply.github.com>
2020-04-10 08:58:01 +0200

    Add files via upload
    WCF Insert

---
+++ IService1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFForInsertValues
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string InsertLandLord(LandLord landinfo);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class LandLord
+    {
+        string vatno = string.Empty;
+        string registrationno = string.Empty;
+        string client = string.Empty;
+        string property = string.Empty;
+        string building = string.Empty;
+        string premises = string.Empty;
+        string meter = string.Empty;
+        double rental;
+        DateTime rentaldate;
+        int rentalecs = 0;
+
+        [DataMember]
+        public string VatNo
+        {
+            get { return vatno; }
+            set { vatno = value; }
+        }
+        [DataMember]
+        public string RegistrationNo
+        {
+            get { return registrationno; }
+            set { registrationno = value; }
+        }
+        [DataMember]
+        public string Client
+        {
+            get { return client; }
+            set { client = value; }
+        }
+        [DataMember]
+        public string Property
+        {
+            get { return property; }
+            set { property = value; }
+        }
+        [DataMember]
+        public string Building
+        {
+            get { return building; }
+            set { building = value; }
+        }
+        [DataMember]
+        public string Premises
+        {
+            get { return premises; }
+            set { premises = value; }
+        }
+        [DataMember]
+        public string Meter
+        {
+            get { return meter; }
+            set { meter = value; }
+        }
+        [DataMember]
+        public Double Rental
+        {
+            get { return rental; }
+            set { rental = value; }
+        } 
+        [DataMember]
+        public DateTime RentalDate
+        {
+            get { return rentaldate; }
+            set { rentaldate = value; }
+        }
+        [DataMember]
+        public int RentalEsc
+        {
+            get { return rentalecs; }
+            set { rentalecs = value; }
+        }
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.Serialization;
+//using System.ServiceModel;
+//using System.ServiceModel.Web;
+//using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WCFForInsertValues
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+
+
+
+    public class Service1 : IService1
+    {
+
+        public string InsertLandLord(LandLord landinfo)
+        {
+            string message = "";
+            SqlConnection con = new SqlConnection("data source = Karabinasql; initial catalog = UPD8_Test; persist security info = False; user id = OS_TestApp; password = Skywalker_J3d1; Max Pool Size = 200; Connection Timeout = 240");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into LandLordInfo(VatNo,RegistrationNo,Client,Property,Building,Premises,Meter,Rental,RentalDate,RentalEsc) VALUES(@VatNo,@RegistrationNo,@Client,@Property,@Building,@Premises,@Meter,@Rental,@RentalDate,@RentalEsc)", con);
+            cmd.Parameters.AddWithValue("@VatNo", landinfo.VatNo);
+            cmd.Parameters.AddWithValue("@RegistrationNo", landinfo.RegistrationNo);
+            cmd.Parameters.AddWithValue("@Client", landinfo.Client);
+            cmd.Parameters.AddWithValue("@Property", landinfo.Property);
+            cmd.Parameters.AddWithValue("@Building", landinfo.Building);
+            cmd.Parameters.AddWithValue("@Premises", landinfo.Premises);
+            cmd.Parameters.AddWithValue("@Meter", landinfo.Meter);
+            cmd.Parameters.AddWithValue("@Rental", landinfo.Rental);
+            cmd.Parameters.AddWithValue("@RentalDate", landinfo.RentalDate);
+            cmd.Parameters.AddWithValue("@RentalEsc", landinfo.RentalEsc);
+
+            int result = cmd.ExecuteNonQuery();
+
+            if(result == 1)
+            {
+                message = landinfo.VatNo + "Details Successfully";
+            }
+            else
+            {
+                message = landinfo.VatNo + "Details not inserted";
+            }
+
+            con.Close();
+            return message;
+        }
+
+    }
+}
